f57cd9efad48c61443c026b910afb540d4007d63
Jeff Doolittle <jeff@jeffdoolittle.com>
2020-08-21 10:59:02 -0700

    --wip-- [skip ci]

---
+++ src/Demo.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.ConsoleApp
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
